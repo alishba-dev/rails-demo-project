2c3772f3c7fd904e2a916aa40e4341c431fb4239
alishba-dev <alishba.saeed@devsinc.com>
2021-03-30 23:38:07 +0500

    Product schema rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_183406) do
+ActiveRecord::Schema.define(version: 2021_03_30_182949) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 2021_03_30_183406) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "sku_number"
   end
 
 end
