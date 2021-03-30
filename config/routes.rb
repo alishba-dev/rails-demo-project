91c81928a5d5b2520fb7a3d586dc75c6e6dd552c
alishba-dev <alishba.saeed@devsinc.com>
2021-03-30 23:30:27 +0500

    Product added by scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :posts #instead of creating each method route for posts separately, we can use this
   root 'posts#index' #now this view will load at start
 end
