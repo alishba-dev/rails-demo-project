3ec436447a94d11e47efea082166a03f94f4b368
alishba-dev <alishba.saeed@devsinc.com>
2021-03-30 23:57:40 +0500

    seeds added for product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+p1 = Product.create!(
+  name: 'Uniliver Product',
+  description: 'Product',
+  sku_number: 'PR1001'
+)
