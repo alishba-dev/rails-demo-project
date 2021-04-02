ce06f928b5191fe035754cd7e75ef78b5419795e
alishba-dev <alishba.saeed@devsinc.com>
2021-04-02 13:12:08 +0500

    Day 3 Grouping Conditional Validations Activerecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_075925) do
+ActiveRecord::Schema.define(version: 2021_04_02_080946) do
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
@@ -110,6 +110,9 @@ ActiveRecord::Schema.define(version: 2021_04_02_075925) do
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_admin"
+    t.string "email"
+    t.string "password"
   end
 
 end
