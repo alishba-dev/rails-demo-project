0b6ad0b53b62fff56146e557929b1753cb50b536
alishba-dev <alishba.saeed@devsinc.com>
2021-04-02 13:03:55 +0500

    Day 3 Conditional Validations Activerecord

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_02_062530) do
+ActiveRecord::Schema.define(version: 2021_04_02_075925) do
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
@@ -53,6 +53,13 @@ ActiveRecord::Schema.define(version: 2021_04_02_062530) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "category_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "payment_type"
+    t.string "card_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "people", force: :cascade do |t|
