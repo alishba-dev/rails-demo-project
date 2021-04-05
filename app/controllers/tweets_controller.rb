af60d104c537782ff0f591247092b2be20054d67
alishba-dev <alishba.saeed@devsinc.com>
2021-04-05 13:19:47 +0500

    Day 4 Flash added

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -10,8 +10,10 @@ class TweetsController < ApplicationController
   def create
     tweet = Tweet.new(tweet_params)
     if tweet.save
+      flash[:notice] = "New Tweet was created."
       redirect_to tweets_index_path
     else
+      flash[:notice] = "Something went wrong."
       redirect_to new_tweets_path
     end
   end
