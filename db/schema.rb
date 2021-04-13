9dd1b6dafcfcc23bbe372258011e61df7e8d027e
alishba-dev <alishba.saeed@devsinc.com>
2021-04-13 18:09:46 +0500

    Session practice

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_071903) do
+ActiveRecord::Schema.define(version: 2021_04_13_120837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "animals", force: :cascade do |t|
     t.string "name"
@@ -32,6 +39,12 @@ ActiveRecord::Schema.define(version: 2021_04_09_071903) do
   end
 
   create_table "authors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "baskets", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -122,9 +135,32 @@ ActiveRecord::Schema.define(version: 2021_04_09_071903) do
     t.string "name"
   end
 
+  create_table "personrails", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "generate"
+    t.string "devise"
+    t.string "Person"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_personrails_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_personrails_on_reset_password_token", unique: true
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "practices", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.integer "random_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
