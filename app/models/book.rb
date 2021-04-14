e17a1c8593a94b7144d7587ae4e5b312dc2f0a88
alishba-dev <alishba.saeed@devsinc.com>
2021-04-14 21:09:57 +0500

    Day 8 scope where implemented

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,10 @@
 class Book < ApplicationRecord
   belongs_to :author, counter_cache: true
+  scope :created_yesterday, -> { where(["created_at >= ?", Book::PARAM[:created_at]] ) }
+  scope :created_whenever, -> (date = DateTime.now - 1.day) { where(["created_at >= ?", date] ) }
+  PARAM = {
+    created_at: DateTime.now - 1.day,
+    updated_at: Time.current - 1.day
+  }
+
 end
