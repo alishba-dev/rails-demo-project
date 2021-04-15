c00ded23e5c498a6762a53f3b3de4002acddcf2c
alishba-dev <alishba.saeed@devsinc.com>
2021-04-15 16:01:07 +0500

    Day 8 association callbacks

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,24 @@
 class Author < ApplicationRecord
-  has_many :books, dependent: :destroy, autosave: true
+  has_many :books, dependent: :destroy, autosave: true,
+              before_add: :my_before_add, after_add: :my_after_add,
+             before_remove: :my_before_remove, after_remove: :my_after_remove
   validates_associated :books
+
+  private
+
+  def my_before_add(book)
+    puts 'I am in before add'
+  end
+
+  def my_after_add(book)
+    puts 'I am in after add'
+  end
+
+  def my_before_remove(book)
+    puts 'I am in before remove'
+  end
+
+  def my_after_remove(book)
+    puts 'I am in after remove'
+  end
 end
