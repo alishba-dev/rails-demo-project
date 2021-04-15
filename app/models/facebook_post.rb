615e05c01b70915b7e7d4147a466f058b49133e1
alishba-dev <alishba.saeed@devsinc.com>
2021-04-15 16:15:44 +0500

    Day 8 association extension

--- app/models/facebook_post.rb
+++ app/models/facebook_post.rb
@@ -1,3 +1,8 @@
 class FacebookPost < ApplicationRecord
+  has_many :facebook_comments do
+    def find_by_id(facebook_comment_id)
+      find_by(id: facebook_comment_id)
+    end
+  end
   has_many :facebook_comments, dependent: :destroy
 end
