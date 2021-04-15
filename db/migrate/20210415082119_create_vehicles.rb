0cfb707b5a3000ca6add907a0c1480abcb3e1cef
alishba-dev <alishba.saeed@devsinc.com>
2021-04-15 13:31:03 +0500

    Day 8 STI vs polymorphism

---
+++ db/migrate/20210415082119_create_vehicles.rb
@@ -0,0 +1,12 @@
+class CreateVehicles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :vehicles do |t|
+      t.string :type, null: false
+      t.string :color
+      t.integer :price
+      t.boolean :purchased, default: false
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_15_075309) do
+ActiveRecord::Schema.define(version: 2021_04_15_082119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -414,6 +414,15 @@ ActiveRecord::Schema.define(version: 2021_04_15_075309) do
     t.boolean "is_admin"
     t.string "email"
     t.string "password"
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "type", null: false
+    t.string "color"
+    t.integer "price"
+    t.boolean "purchased", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "workers", force: :cascade do |t|
