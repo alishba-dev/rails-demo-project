673c1cc6aa435ba3317cfd708b0c369013f652e9
alishba-dev <alishba.saeed@devsinc.com>
2021-04-16 12:44:00 +0500

    Day 8 Pessimistic Locking

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,16 @@
 class Account < ApplicationRecord
   belongs_to :supplier
   has_one :account_history
+
+  # def perform_changes
+  #   transaction do
+  #     abc = Account.lock('FOR UPDATE NOWAIT').find_by(name: 'abc')
+  #     xyz = Account.lock('FOR UPDATE NOWAIT').find_by(name: 'xyz')
+  #     abc.name = 'ABC'
+  #     abc.save!
+  #     xyz.name = 'XYZ'
+  #     xyz.save!
+  #   end
+
+  end
 end
